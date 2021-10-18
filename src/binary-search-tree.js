744256acf7491458dc437f1a6fb4ae08db851f55
BertFrontEnd <Berton@yandex.ru>
2021-10-18 21:35:51 +0300

    feat: add add-method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+/* const { NotImplementedError } = require('../extensions/index.js'); */
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
  * Implement simple binary search tree according to task description
@@ -15,9 +15,23 @@ module.exports = class BinarySearchTree {
     return this.roots;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    function insertNode(node, newData) {
+      if (node === null) {
+        return new Node(newData);
+      }
+      if (node.data === newData) {
+        return node;
+      }
+      if (newData < node.data) {
+        node.left = insertNode(node.left, data);
+      } else {
+        node.right = insertNode(node.right, data);
+      }
+      return node;
+    }
+
+    this.roots = insertNode(this.roots, data);
   }
 
   has(/* data */) {
